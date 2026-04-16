05e45860188c7a81838c836a5969948d3b8c0236
Shane Mensah <papakwasimensah@gmail.com>
2026-04-16 09:04:28 +0000

    Change in the Schema

--- database/migrations/2026_04_15_204637_fix_policies_unique_constraint.php
+++ database/migrations/2026_04_15_204637_fix_policies_unique_constraint.php
@@ -14,12 +14,6 @@ return new class extends Migration
         Schema::table('policies', function (Blueprint $table) {
             // Add insured_name column
             $table->string('insured_name')->nullable()->after('policy_number');
-
-            // Drop external_policy_id unique (we added this earlier)
-            $table->dropUnique('policies_external_policy_id_unique');
-
-            // Make policy_number the unique key
-            $table->unique('policy_number', 'policies_policy_number_unique');
         });
     }
 
@@ -30,8 +24,6 @@ return new class extends Migration
     {
         Schema::table('policies', function (Blueprint $table) {
             $table->dropColumn('insured_name');
-            $table->dropUnique('policies_policy_number_unique');
-            $table->unique('external_policy_id', 'policies_external_policy_id_unique');
         });
     }
 };
