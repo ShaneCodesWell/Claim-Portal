2e17df3e244c49ff85f6e18eef4e53ed5305141a
Shane Mensah <shanecodes69@gmail.com>
2026-07-01 11:57:38 +0000

    Restoring the password field for the staff

--- resources/views/admin/organization/staff/edit.blade.php
+++ resources/views/admin/organization/staff/edit.blade.php
@@ -121,7 +121,7 @@
                             </template>
                         </div>
                     </div>
-                    {{-- <div>
+                    <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Password *</label>
                         <input type="password" name="password" required
                             class="w-full border border-gray-300 rounded-lg px-3 py-2">
@@ -130,7 +130,7 @@
                         <label class="block text-sm font-medium text-gray-700 mb-1">Confirm Password *</label>
                         <input type="password" name="password_confirmation" required
                             class="w-full border border-gray-300 rounded-lg px-3 py-2">
-                    </div> --}}
+                    </div>
                 </div>
                 <div class="md:col-span-2">
                     <label class="block text-sm font-medium text-gray-700 mb-2">
