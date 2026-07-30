802f95b01dd09d8ec2af94a6f70da6c7833816a7
Shane Mensah <shanecodes69@gmail.com>
2026-07-30 18:15:37 +0000

    Update to the seeder

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -14,7 +15,7 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         $company    = Company::first();
-        $headOffice = Branch::where('code', 'ACC-HQ')->first();
+        $headOffice = Branch::where('code', '1000')->first();
 
         $departments = [
             [
